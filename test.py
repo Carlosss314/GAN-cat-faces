f9f6f2fdb7d733d2b6a657c4c5d867461f718cbc
Carlosss314 <gross.masse@gmail.com>
2024-07-05 18:29:49 +0200

    Update test.py

--- test.py
+++ test.py
@@ -7,21 +7,22 @@ import matplotlib.pyplot as plt
 images = torch.load("dataset.pt")
 
 G = generator()
-G = torch.load("model_parameters/Generator_epoch_250.pt")
-# G = torch.load("model_parameters2/Generator_epoch_105.pt")
+G = torch.load("model_parameters/Generator_epoch_255.pt", map_location=torch.device('cpu'))
 
-
-noise = (torch.rand(100, 128) - 0.5) / 0.5
+batch_size = 64
+noise = (torch.rand(batch_size, 128, 1, 1) - 0.5) / 0.5
 fake_imgs = G(noise)
 
-fake_imgs = fake_imgs.reshape(100, 64, 64)
 
 for i in range(fake_imgs.shape[0]):
-    fake_img = fake_imgs[i].detach()
+    img = images[i].permute(1, 2, 0)
+    img = ((img + 1) * (255/2)).int() #reverse normalization
+    fake_img = fake_imgs[i].detach().permute(1, 2, 0)
+    fake_img = ((fake_img + 1) * (255/2)).int() #reverse normalization
     plt.subplot(1, 2, 1)
-    plt.imshow(images[i], cmap="gray")
+    plt.imshow(img)
     plt.title("a real image")
     plt.subplot(1, 2, 2)
-    plt.imshow(fake_img, cmap="gray")
+    plt.imshow(fake_img)
     plt.title("a generated image")
     plt.show()
