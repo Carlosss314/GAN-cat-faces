e5943066e43a625db114fb57e943915899a641a9
Carlosss314 <gross.masse@gmail.com>
2024-07-05 18:29:36 +0200

    Update model.py

--- model.py
+++ model.py
@@ -3,27 +3,85 @@ import torch.nn as nn
 class discriminator(nn.Module):
     def __init__(self):
         super(discriminator, self).__init__()
-        self.fc1 = nn.Linear(4096, 512)
-        self.fc2 = nn.Linear(512, 1)
-        self.activation = nn.LeakyReLU(0.1)
+        self.conv1 = nn.Sequential(
+            # in: 3 x 64 x 64
+            nn.Conv2d(3, 64, kernel_size=4, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(64),
+            nn.LeakyReLU(0.2, inplace=True),
+            # out: 64 x 32 x 32
+        )
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(64, 128, kernel_size=4, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(128),
+            nn.LeakyReLU(0.2, inplace=True),
+            # out: 128 x 16 x 16
+        )
+        self.conv3 = nn.Sequential(
+            nn.Conv2d(128, 256, kernel_size=4, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(256),
+            nn.LeakyReLU(0.2, inplace=True),
+            # out: 256 x 8 x 8
+        )
+        self.conv4 = nn.Sequential(
+            nn.Conv2d(256, 512, kernel_size=4, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(512),
+            nn.LeakyReLU(0.2, inplace=True),
+            # out: 512 x 4 x 4
+        )
+        self.conv5 = nn.Sequential(
+            nn.Conv2d(512, 1, kernel_size=4, stride=1, padding=0, bias=False),
+            # out: 1 x 1 x 1
+        )
 
     def forward(self, x):
-        x = x.view(-1, 4096)
-        x = self.activation(self.fc1(x))
-        x = self.fc2(x)
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = self.conv3(x)
+        x = self.conv4(x)
+        x = self.conv5(x)
+        x = nn.Flatten()(x)
         return nn.Sigmoid()(x)
+
+
 
 class generator(nn.Module):
     def __init__(self):
         super(generator, self).__init__()
-        self.fc1 = nn.Linear(128, 1024)
-        self.fc2 = nn.Linear(1024, 2048)
-        self.fc3 = nn.Linear(2048, 4096)
-        self.activation = nn.ReLU()
+        self.conv1 = nn.Sequential(
+            # in: latent_size x 1 x 1
+            nn.ConvTranspose2d(128, 512, kernel_size=4, stride=1, padding=0, bias=False),
+            nn.BatchNorm2d(512),
+            nn.ReLU(True),
+            # out: 512 x 4 x 4
+        )
+        self.conv2 = nn.Sequential(
+            nn.ConvTranspose2d(512, 256, kernel_size=4, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(256),
+            nn.ReLU(True),
+            # out: 256 x 8 x 8
+        )
+        self.conv3 = nn.Sequential(
+            nn.ConvTranspose2d(256, 128, kernel_size=4, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(128),
+            nn.ReLU(True),
+            # out: 128 x 16 x 16
+        )
+        self.conv4 = nn.Sequential(
+            nn.ConvTranspose2d(128, 64, kernel_size=4, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(64),
+            nn.ReLU(True),
+            # out: 64 x 32 x 32
+        )
+        self.conv5 = nn.Sequential(
+            nn.ConvTranspose2d(64, 3, kernel_size=4, stride=2, padding=1, bias=False),
+            nn.Tanh()
+            # out: 3 x 64 x 64
+        )
 
     def forward(self, x):
-        x = self.activation(self.fc1(x))
-        x = self.activation(self.fc2(x))
-        x = self.fc3(x)
-        x = x.view(-1, 64, 64)
-        return nn.Tanh()(x)
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = self.conv3(x)
+        x = self.conv4(x)
+        x = self.conv5(x)
+        return x
